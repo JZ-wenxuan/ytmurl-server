e445578d51b95ba26fe9da510e0fc1221fc83bdb
Wenxuan Zhu <jack.zwx@icloud.com>
2023-02-27 20:01:32 -0600

    initial commit

---
+++ api/index.py
@@ -0,0 +1,25 @@
+from http.server import BaseHTTPRequestHandler
+from ytmurl.get import get as get_ytmrul
+from urllib.parse import urlparse, parse_qs
+
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    parsed_url = urlparse(self.path)
+    if parsed_url.path != '/song':
+      self.send_response(404)
+      return
+
+    # retrieve query
+    query = parse_qs(parsed_url.query)
+    try:
+      response = get_ytmrul(query['q'][0], (int(query['dmin'][0]), int(query['dmax'][0])))
+    except:
+      self.send_response(500)
+      return
+
+    # Send the HTML message
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(response.encode())
+    return
